78c8af85ae0d7528f21b85d1bac480c499f07ba4
Kartik Khiriya <kartikkhiriya3@gmail.com>
2025-04-16 22:28:02 +0530

    added new files

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import Heart from '../assets/Heart'
 
 const Footer = () => {
   return (
-    <div className='gap-5 text-3xl  w-full h-34 bg-slate-300 backdrop-blur-2xl shadow-2xl text-black flex justify-center items-center'>
+    <div className=' select-none gap-5 text-3xl  w-full h-34 bg-slate-300 backdrop-blur-2xl shadow-2xl text-black flex justify-center items-center'>
       Made With <Heart /> by Kartik
     </div>
   )
